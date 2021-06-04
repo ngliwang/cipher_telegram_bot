fe2b3cb56042c0cfc6a679d272f5fe926a180120
ngliwang <59732664+ngliwang@users.noreply.github.com>
2021-06-04 14:35:34 +0800

    commit on 'main'

--- hello_world.java
+++ hello_world.java
@@ -1,4 +1,6 @@
 // Your First Program
+// vscode://vscode.github-authentication/did-authenticate?windowid=1&code=ce821492b8513a00d9fc&state=256d8844-7fef-4edf-b6ed-de4dc2b4a48a
+
 
 class HelloWorld {
     public static void main(String[] args) {
